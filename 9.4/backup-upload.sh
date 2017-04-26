7d242278c29af333b77b6a2a8d03ddb7a6bc2dff
Don Nguyen <don.nguyen@hazuu.com>
2017-04-26 16:19:25 +0700

    Echo some statuses

--- 9.4/backup-upload.sh
+++ 9.4/backup-upload.sh
@@ -6,11 +6,15 @@ dname="/data/"
 rm -rf "$dname"
 mkdir -p "$dname"
 
+echo 'BK----------------- Backing up database file...'
 /bin/bash backup-db.sh "$dname$fname"
+echo 'BK----------------- Backed up'
 
+echo 'BK----------------- Uploading database file to S3...'
 /bin/bash s3setup.sh
 cd "$dname"
-/usr/bin/s3cmd put --rr "$fname" s3://$AWS_S3_PATH$fname
+/usr/bin/s3cmd put --rr --progress "$fname" $AWS_S3_PATH$fname
+echo 'BK----------------- Uploaded database file to S3'
 
 cd /
 rm -rf "$dname"
--- 9.4/restore.sh
+++ 9.4/restore.sh
@@ -4,12 +4,16 @@ set -e
 dname="/data/"
 rm -rf "$dname"
 mkdir "$dname"
+echo 'RS----------------- Downloading database file...'
 curl "$1" | zcat > "${dname}restore-me.sql"
+echo 'RS----------------- Downloaded database file'
 
+
+echo 'RS----------------- Restoring database...'
 PGHOST="$POSTGRES_PORT_5432_TCP_ADDR" \
 PGPORT="$POSTGRES_PORT_5432_TCP_PORT" \
 PGUSER="$POSTGRES_ENV_POSTGRES_USER" \
 PGPASSWORD="$POSTGRES_ENV_POSTGRES_PASSWORD" \
 pg_restore -Fc --no-owner --dbname "$DBNAME" -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -w "${dname}restore-me.sql"
-
+echo 'RS----------------- Restored!'
 # rm -rf "$dname"
